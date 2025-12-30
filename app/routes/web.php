74ecee1be9a3389ddaadc48db40405977b4a848b
longduc205 <longdcuk0205@gmail.com>
2025-12-30 23:14:26 +0700

    [ADD] Thêm route cơ bản ban đầu.

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [PageController::class, 'home']);
+
+Route::get('/hello', function () {
+    return 'Hello Laravel Docker!';
 });
